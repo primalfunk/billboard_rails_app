3ccb39e1110e6a00be62ae83649b7225fa7885cc
Alex Winitzky <alex.winitzky@gmail.com>
2018-05-31 16:08:12 -0600

    routes

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,13 +1,50 @@
 class ChartsController < ApplicationController
-  def index
-  end
+  before_action :set_chart, only: [:show, :update, :edit, :destroy]
 
-  def new
+  def index
+    @charts = Chart.all
   end
 
   def show
   end
 
+  def new
+    @chart = Chart.new
+  end
+
   def edit
   end
+
+  def create
+    @chart = Chart.new(chart_params)
+    
+    if @chart.save
+      redirect_to charts_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @chart.update(charts_params)
+      redirect_to @chart
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @chart.destroy
+    redirect_to charts_path
+  end
+
+  private
+    
+    def set_chart
+      @chart = Chart.find(params[:id])
+    end
+
+    def chart_params
+      params.require(:chart).permit(:title)
+    end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,16 @@
 class SongsController < ApplicationController
+  before_action :set_artist
+  before_action: :set_chart
+  
   def show
   end
+
+  private
+    def set_artist
+      @artist = Artist.find(params[:id])
+    end
+
+    def set_chart
+      @chart = Chart.find(params[:id])
+    end
 end
